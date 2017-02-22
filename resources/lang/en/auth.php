dec4064d60b0a4882ebed9fb7aeeb84f0b91d10f
YohanArnoux69 <yarnoux69@gmail.com>
2017-02-22 17:26:38 +0100

    Changement test langue
    bite

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Vos informations ne correspondent pas.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
