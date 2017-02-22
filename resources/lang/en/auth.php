5c4b06c4b870489f549dc4e0c1e4c0e392f8e2b3
YohanArnoux69 <yarnoux69@gmail.com>
2017-02-22 17:48:29 +0100

    NIQUE
    TAMERE

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Vos informations ne correspondent pas.',
+    'throttle' => 'Essais limités. Réessayez dans :seconds secondes.',
 
 ];
